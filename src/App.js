798087f7f0424d7e2ae4fc120d169b4312941c29
Nandlal Mahto <nandmahto1512000.com>
2023-05-29 17:45:49 +0530

    added navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 // import logo from './logo.svg';3
 import './App.css';
+import Navbar from './Components/navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>YouTube</h1>
+        <Navbar />
     </div>
   );
 }
